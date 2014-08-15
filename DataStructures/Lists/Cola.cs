1367c3b6aef6fac6743ade8bcb486b84d930603b
Ravila <ravila@nearbpo.com>
2014-08-15 12:53:11 -0500

    add Coal and Triangulo de Pascual

---
+++ DataStructures/Lists/Cola.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace DataStructures.Lists
+{
+    public class Morro
+    {
+        public Morro(string apodo)
+        {
+            Apodo = apodo;
+        }
+
+        public Morro Siguiente { get; set; }
+
+        public string Apodo { get; set; } 
+    }
+
+    public class ColaDeLasTortillas
+    {
+        private static Morro _apuntador;
+
+        public Morro Apuntador
+        {
+            get { return _apuntador; }
+        }
+
+        public void MandarAlMorror  (string chamaco)
+        {
+            if (_apuntador == null)
+            {
+                _apuntador = new Morro(chamaco);
+                return;
+            }
+
+            EncolarMorro(_apuntador, chamaco);
+        }
+
+        private void EncolarMorro(Morro apuntador, string chamaco)
+        {
+            if (apuntador.Siguiente != null)
+            {
+                EncolarMorro(apuntador.Siguiente, chamaco);
+            }
+            else
+            {
+                apuntador.Siguiente =new Morro(chamaco);
+            }
+        }
+
+        public string AtenderAlMorroSiguiente()
+        {
+            if (_apuntador != null)
+            {
+                var chamaco = _apuntador.Apodo;
+                if (_apuntador.Siguiente == null)
+                {
+                    _apuntador = null;
+                }
+                else
+                {
+                    _apuntador.Apodo = _apuntador.Siguiente.Apodo;
+                    _apuntador.Siguiente = _apuntador.Siguiente.Siguiente;
+                }
+
+                return chamaco;
+            }
+            
+            return null;
+        }
+    }
+
+    [TestFixture]
+    public class Torilleria
+    {
+        [Test]
+        public void Trabajar()
+        {
+            var colaDeLasTortillas = new ColaDeLasTortillas();
+
+            colaDeLasTortillas.MandarAlMorror("joan");
+            colaDeLasTortillas.MandarAlMorror("rafail");
+
+            var morroAtendido = colaDeLasTortillas.AtenderAlMorroSiguiente();
+            Console.WriteLine(morroAtendido);
+
+            var morroAtendido1 = colaDeLasTortillas.AtenderAlMorroSiguiente();
+            Console.WriteLine(morroAtendido1);
+
+            var morroAtendido2 = colaDeLasTortillas.AtenderAlMorroSiguiente();
+            Console.WriteLine(morroAtendido2);
+        }
+    }
+}
---
+++ DataStructures/Problemos/TranguloDePascual.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace DataStructures.Problemos
+{
+    public class TranguloDePascual
+    {
+        private int _niveles;
+
+        private List<int[]> _triangulo;
+
+        public TranguloDePascual(int niveles)
+        {
+            _niveles = niveles;
+        }
+
+        public int Niveles
+        {
+            get { return _niveles; }
+        }
+
+        public List<int[]> GenerarTriangulazo()
+        {
+            
+            if (_niveles <= 1)
+            {
+                return _triangulo;
+            }
+
+            _triangulo = new List<int[]>();
+            var triangulo1 = new[] { 1 };
+            _triangulo.Add(triangulo1);
+
+            Generar(triangulo1);
+            
+            return _triangulo;
+        }
+
+        void Generar(int[] triangulo)
+        {
+            if (triangulo.Length == (_niveles +1))
+            {
+                return;
+            }
+
+            var nuevo = new List<int>();
+            nuevo.Add(1);
+
+            for (int i = 1; i < triangulo.Length; i++)
+            {
+                int suma = triangulo[i - 1] + triangulo[i];
+                nuevo.Add(suma);
+            }
+
+            nuevo.Add(1);
+            _triangulo.Add(nuevo.ToArray());
+            Generar(nuevo.ToArray());
+        }
+    }
+
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            var pascual = new TranguloDePascual(3);
+            List<int[]> triangulazo = pascual.GenerarTriangulazo();
+        }
+    }
+}
